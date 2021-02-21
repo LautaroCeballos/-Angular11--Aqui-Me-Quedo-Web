6ff333804c038dc9a7b6a43ceb6bd0ce2b0cb425
Lautaro <lautaroceballos2013@gmail.com>
2021-02-20 22:08:54 -0300

    (UPDATE) Slider-Home funcional y responsive

--- src/app/components/slider-home/slider-home.component.ts
+++ src/app/components/slider-home/slider-home.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { trigger, transition, useAnimation } from "@angular/animations";
 import { fadeIn, fadeOut } from "../../animations/animations";
-
-
 
 @Component({
   selector: 'app-slider-home',
@@ -10,8 +9,8 @@ import { fadeIn, fadeOut } from "../../animations/animations";
   styleUrls: ['./slider-home.component.css'],
   animations: [
     trigger('carouselAnimation', [
-      transition('void => *', [useAnimation(fadeIn, {params: {time: '1300ms'}})]),
-      transition('* => void', [useAnimation(fadeOut, {params: {time: '1300ms'}})])
+      transition('void => *', [useAnimation(fadeIn, { params: { time: '1300ms' } })]),
+      transition('* => void', [useAnimation(fadeOut, { params: { time: '1300ms' } })])
     ])
   ]
 })
@@ -19,13 +18,18 @@ import { fadeIn, fadeOut } from "../../animations/animations";
 
 export class SliderHomeComponent implements OnInit {
 
+  faAngleLeft = faAngleLeft;
+  faAngleRight = faAngleRight;
+
   public slides = [
-    { src: "../../../assets/images/slider-home/img1.jpg" },
-    { src: "../../../assets/images/slider-home/img2.jpg" },
-    { src: "../../../assets/images/slider-home/img3.jpg" },
-    { src: "../../../assets/images/slider-home/img4.jpg" },
-    { src: "../../../assets/images/slider-home/img5.jpg" }
+    { src: "../../../assets/images/slider-home/img1.jpg", alt: "", id: 0 },
+    { src: "../../../assets/images/slider-home/img2.jpg", alt: "", id: 1 },
+    { src: "../../../assets/images/slider-home/img3.jpg", alt: "", id: 2 },
+    { src: "../../../assets/images/slider-home/img4.jpg", alt: "", id: 3 },
+    { src: "../../../assets/images/slider-home/img5.jpg", alt: "", id: 4 }
   ];
+
+  public slidesThumbs = this.slides.slice();
 
   currentSlide = 0;
 
@@ -33,26 +37,66 @@ export class SliderHomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.preloadImages();
+    this.updateSlideThumb("default");
   }
 
   preloadImages() {
-    for(const slide of this.slides){
+    for (const slide of this.slides) {
       new Image().src = slide.src;
     }
   }
 
-  onPreviousClick(){
-    const previous = this.currentSlide -1;
-    this.currentSlide = previous < 0 ? this.slides.length -1 : previous;
-    console.log("previous clicked, new current slide is: ", this.currentSlide);
+  trackByItems(index: number, item: any){
+    return index;
   }
 
-  onNextClick(){
+  onPreviousClick() {
+    this.updateSlideThumb("previous");
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+  }
+
+  onNextClick() {
+    this.updateSlideThumb("next");
     const next = this.currentSlide + 1;
     this.currentSlide = next === this.slides.length ? 0 : next;
-    console.log("next clicked, new current slide is: ", this.currentSlide);
   }
-  
 
 
+  slideTo(thumbId, index) {
+    this.updateSlideThumb("to", index);
+    this.currentSlide = thumbId;
+  }
+
+  updateSlideThumb(action: string, index?:Number) { //Sobrecarga de metodos
+    switch (action) {
+      case "previous":
+        this.slidesThumbs.pop();
+        this.slidesThumbs.unshift(this.slides[this.currentSlide]);
+        break;
+
+      case "next":
+        this.slidesThumbs.shift();
+        this.slidesThumbs.push(this.slides[this.currentSlide]);
+        break;
+
+      case "to":
+        for(let i = 0; i <= index; i++){
+          this.slidesThumbs.shift();
+          this.slidesThumbs.push(this.slides[this.currentSlide]);
+          const next = this.currentSlide + 1;
+          this.currentSlide = next === this.slides.length ? 0 : next;
+        }
+        break;
+
+      default:
+        this.slidesThumbs = this.slides.slice();
+        this.slidesThumbs.splice(this.currentSlide, 1);
+        break;
+    }
+  }
+
+  slideToMobile(index){
+    this.currentSlide = index;
+  }
 }
